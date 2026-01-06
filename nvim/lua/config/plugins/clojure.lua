9dd55f6b7fa41f70f2bb6445fe4d0cc8c1727043
Olli Varila <olli.varila@gmail.com>
2026-01-06 13:34:08 +0200

    Fix clojure loading

--- nvim/lua/config/plugins/clojure.lua
+++ nvim/lua/config/plugins/clojure.lua
@@ -1,23 +1,26 @@
+local enable_clojure = false
+
 return {
-  'Olical/conjure',
+  {
+    'Olical/conjure',
+    enabled = enable_clojure,
+    ft = { 'clojure' },
+    config = function()
+      require('config.keymaps').clojure()
+    end,
+  },
   {
     'clojure-vim/vim-jack-in',
+    enabled = enable_clojure,
     dependencies = {
       'tpope/vim-dispatch',
     },
   },
-  -- 'luochen1990/rainbow',
-  -- 'HiPhish/rainbow-delimiters.nvim',
   {
-
     'tpope/vim-sexp-mappings-for-regular-people',
+    enabled = enable_clojure,
     dependencies = {
       'guns/vim-sexp',
     },
   },
-  ft = { 'clojure' },
-  config = function()
-    require('config.keymaps').clojure()
-  end,
-  enabled = false,
 }
