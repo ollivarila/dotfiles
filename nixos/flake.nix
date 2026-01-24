08355000bf4dd2c465f99d6e03eb14fb37daa429
Olli Varila <olli.varila@gmail.com>
2026-01-25 01:51:39 +0200

    Update nixpkgs

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,9 +1,9 @@
 {
   description = "NixOs config";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -21,13 +21,7 @@
       nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
         inherit system;
         modules = [
-          # home-manager.nixosModules.home-manager
           ./configuration.nix
-          # {
-          #   home-manager.useGlobalPkgs = true;
-          #   home-manager.useUserPackages = true;
-          #   home-manager.users.${user} = import ./home.nix;
-          # }
         ];
       };
       homeConfigurations = {
