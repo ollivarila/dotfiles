64a73e6efe706b13a69999853f12e3cba5bd116c
Olli Varila <olli.varila@gmail.com>
2026-06-30 09:59:43 +0300

    Remove autolisttab

--- nvim/lua/config/plugins/markdown.lua
+++ nvim/lua/config/plugins/markdown.lua
@@ -11,7 +11,7 @@ return {
     config = function()
       require('autolist').setup()
 
-      vim.keymap.set('i', '<tab>', '<cmd>AutolistTab<cr>')
+      -- vim.keymap.set('i', '<tab>', '<cmd>AutolistTab<cr>')
       vim.keymap.set('i', '<s-tab>', '<cmd>AutolistShiftTab<cr>')
       -- vim.keymap.set("i", "<c-t>", "<c-t><cmd>AutolistRecalculate<cr>") -- an example of using <c-t> to indent
       vim.keymap.set('i', '<CR>', '<CR><cmd>AutolistNewBullet<cr>')
