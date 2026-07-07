a02c6a6e1d94abe3c15cc68f983a9a55894ed2a7
Olli Varila <olli.varila@gmail.com>
2026-07-07 21:08:34 +0300

    Point mason plugins at mason-org, fix ensure_installed merge bug
    williamboman/mason* repos moved orgs. Also list_extend instead of
    tbl_extend, which was silently dropping ts_ls/eslint on non-work setups

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,9 +4,6 @@ local autocmds = require 'config.autocmds'
 
 local utils = require 'config.utils'
 
--- FIXME: deprecated/aging config to modernize (nvim 0.12):
--- FIXME: [3] mason repos moved to mason-org/; williamboman/* archived (lsp.lua:950-952)
-
 -- NOTE: Default settings contain settings that are plugin agnostic
 -- They also might need to be loaded before any plugins
 
--- nvim/lua/config/plugins/lsp.lua
+++ nvim/lua/config/plugins/lsp.lua
@@ -6,9 +6,9 @@ return {
   'neovim/nvim-lspconfig',
   dependencies = {
     -- Package manager for lsps
-    'williamboman/mason.nvim',
+    'mason-org/mason.nvim',
     -- Automatic lsp install & setup
-    'williamboman/mason-lspconfig.nvim',
+    'mason-org/mason-lspconfig.nvim',
     -- Useful status updates for LSP.
     { 'j-hui/fidget.nvim', opts = {} },
   },
@@ -76,10 +76,7 @@ return {
     local utils = require 'config.utils'
 
     if not utils.is_work() then
-      ensure_installed = vim.tbl_extend('force', ensure_installed, {
-        'clojure_lsp',
-        'nil_ls',
-      })
+      vim.list_extend(ensure_installed, { 'clojure_lsp', 'nil_ls' })
     end
 
     -- This plugin installs all the listed lsps, configures and enables them
