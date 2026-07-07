5810a4ec12dc752dd7721919c4fab7365bf64030
Olli Varila <olli.varila@gmail.com>
2026-07-07 20:23:00 +0300

    Remove deprecated function

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -10,7 +10,7 @@ return {
       desc = 'Highlight when yanking (copying) text',
       group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
       callback = function()
-        vim.highlight.on_yank()
+        vim.hl.on_yank()
       end,
     })
     vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
